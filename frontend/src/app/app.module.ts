54d59a92e4912b7c7d621545bf510a16ba23a152
Michael Cruz <michaelcruz.dev@gmail.com>
2024-04-25 11:30:14 -0300

    Iniciado o frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './shared/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardInicioComponent } from './components/card-inicio/card-inicio.component';
+import { CadastroFormComponent } from './components/cadastro-form/cadastro-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent,
+    CardInicioComponent,
+    CadastroFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/components/card-inicio/card-inicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-inicio',
+  templateUrl: './card-inicio.component.html',
+  styleUrls: ['./card-inicio.component.css']
+})
+export class CardInicioComponent {
+
+}
